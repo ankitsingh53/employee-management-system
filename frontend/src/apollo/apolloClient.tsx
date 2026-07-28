621ec9657ef5fb933fb030547305dc078f376014
Ankit Kumar Singh <ankit.ks@celestialsys.com>
2026-07-29 01:36:16 +0530

    made frontend and backend connection

--- frontend/src/apollo/apolloClient.tsx
+++ frontend/src/apollo/apolloClient.tsx
@@ -2,8 +2,9 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:3000/graphql",
+    uri: import.meta.env.VITE_GRAPHQL_URL,
     credentials: "include",
+     headers: {'apollo-require-preflight': "true"}
   }),
   cache: new InMemoryCache(),
 });
